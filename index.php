49591b9a77e5e47e45f6cd7869a913f2c03a662a
53318214M <53318214M@A32O22.novaulas.stucom.com>
2016-11-17 17:41:09 +0100

    ESCRITO DATOS VISUALES

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ and open the template in the editor.
     </head>
     <body>
         <form action="calcula.php" method="POST">
-            <input type="number" name="Edad1" id="">
-            <input type="number" name="Edad2" id="">
-            <input type="number" name="Edad3" id="">
-            <input type="number" name="Edad4" id="">
-            <input type="number" name="Edad5" id="">
-            <input type="number" name="Edad6" id="">	
-            <input type="submit">
+           Edad 1: <input type="number" name="Edad1" id=""><br>
+           Edad 2: <input type="number" name="Edad2" id=""><br>
+           Edad 3: <input type="number" name="Edad3" id=""><br>
+           Edad 4: <input type="number" name="Edad4" id=""><br>
+           Edad 5: <input type="number" name="Edad5" id=""><br>
+           Edad 6: <input type="number" name="Edad6" id=""><br>	
+           <input type="submit">
 	</form>
     </body>
 </html>
