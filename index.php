81f85801d5b6ac9d8d169ccee5773f1c9571267f
53318214M <53318214M@A32O22.novaulas.stucom.com>
2016-11-17 17:34:08 +0100

    FORMULARIO CON 6 CAMPOS DE EDAD

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="calcula.php" method="POST">
+			<input type="number" name="Edad1" id="">
+			<input type="number" name="Edad2" id="">
+			<input type="number" name="Edad3" id="">
+			<input type="number" name="Edad4" id="">
+			<input type="number" name="Edad5" id="">
+			<input type="number" name="Edad6" id="">	
+	</form>
+    </body>
+</html>
