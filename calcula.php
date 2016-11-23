fae3f6caf82877db298c39082237eba926332b90
53318214M <53318214M@A32O22.novaulas.stucom.com>
2016-11-23 18:45:26 +0100

    ACABADO EJERCICIO Y ORDENADO

--- calcula.php
+++ calcula.php
@@ -17,15 +17,25 @@ and open the template in the editor.
         $edades[] = $POST_['Edad4'];
         $edades[] = $POST_['Edad5'];
         $edades[] = $POST_['Edad6'];
-        
         $sumeo = 0;
+        $media = $suma/count($edades);
+        $par=0;
+        
         foreach ($edades as $num){
             $sumeo = $sumeo+$num;
         }
         echo "El resultado es $sumeo";
         
-        $media = $suma/count($valor);
+        
         echo "<br> La media es $media";
+        
+        
+        for($i=0;$i<count($edades);$i++){
+                if($edades[$i]%2==0){
+                $par++;    
+                }
+        }
+        echo "<br>El numero de pares es: $par";
         ?>
     </body>
 </html>
